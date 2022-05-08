fc61f90ee0bb53ee06254805305048d005aad499
Satabrata Paul <satabrata.slg88@gmail.com>
2022-05-09 01:23:42 +0530

    String manipulation

--- guess_game/src/main.rs
+++ guess_game/src/main.rs
@@ -1,6 +1,9 @@
 use std::io;
 
 fn main() {
+    let x = 5;
+    let y = 8;
+
     println!("Welcome to guess game.....");
     println!();
     println!("Guess the number!!!");
@@ -12,4 +15,6 @@ fn main() {
     .expect("Failed to take input");
 
     println!("Your guess is : {}", user);
+    println!();
+    println!("The values of x and y are : {} and {} respectively", x, y);
 }
