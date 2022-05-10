2682e79e60e078ae31f38de44c1ed253c2741242
Satabrata Paul <satabrata.slg88@gmail.com>
2022-05-10 17:33:39 +0530

    mut keyword

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,4 +3,11 @@ fn main()
     //noral variable declaration is immutable by default
     let x = 10;
     println!("Immutable variable x has value: {}", x);
+
+    //We can make it mutable using mut
+    let mut y = 10;
+    println!("Value of y before mutation: {}", y);
+
+    y = 20;
+    println!("Value of y after mutation: {}", y);
 }
