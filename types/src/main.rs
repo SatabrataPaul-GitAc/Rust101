9e98d3176fc3ed1ad53f24f10b643d03e4141840
Satabrata Paul <satabrata.slg88@gmail.com>
2022-05-14 08:49:21 +0530

    integer and float type

---
+++ types/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let num1 = 10; //By default rust assumes i32 type
+
+    let num2: u32 = 36; //Type annotattion , specifying type of unsigned 32 integer
+
+    let num3 = 3.6; //By default rust assumes f64 type in case of floating point numbers
+
+    let num4 = 9.5f32; //We can specify the type as a suffix to the value as well
+}
